cdd361f0a67afbbb96e4281f3a2533a0e3022782
joe <presbrey+git@gmail.com>
2014-01-29 21:31:27 -0500

    also use default path: /usr/include/raptor2

--- goraptor.go
+++ goraptor.go
@@ -63,7 +63,7 @@ binary encoding for use with the gob package.
 */
 package goraptor
 
-// #cgo CFLAGS: -I/usr/local/include/raptor2
+// #cgo CFLAGS: -I/usr/local/include/raptor2 -I/usr/include/raptor2
 // #cgo LDFLAGS: -L/usr/local/lib -lraptor2
 // #include <stdlib.h>
 // #include <string.h>
